3cd17243b95280781780db751e8d986d158878ef
Nikhil-071996 <nikhilkachi68@gmail.com>
2023-05-02 13:33:22 +0530

    changes in path

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { BrowserRouter , Route , Routes} from 'react-router-dom';
 function App() {
     
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/MovieAmaze-'>
       <div className="App">
         <Navigation/>
         <Routes>
